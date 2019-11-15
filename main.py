c3009889ae8f455db86486d07469a03cf958073f
Amgalan <darzhaev@gmail.com>
2019-11-15 21:59:39 +0900

    Вынесен скрипт в отдельный файл(модуль)

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from winery_age import get_winery_age
-
-
-with open("action.txt", "r", encoding='utf-8-sig') as rose_file:
-	rose_list= rose_file.read()
-eng_list= rose_list.replace('Название: ', 'title:').replace('Сорт: ', 'sort:').replace('Цена: ', 'price:')
-list_by_branch = eng_list.replace('Картинка: ','image:').replace('Выгодное предложение','action:True').split('#')
-list_by_branch.remove('')
-production = {}
-for branch_txt in list_by_branch:
-	branch_list= branch_txt.split('\n\n\n')
-	product_list_data = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
-	all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list_data]
-	production.update({branch_list[0]:all_tag_dict})
-
+from format_list import get_production
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -24,7 +11,7 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    winery_age=get_winery_age(), production=production)
+    winery_age=get_winery_age(), production=get_production())
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
