0e51a89a6f2f738fdd90fb1631712b8ae9bcfc07
Amgalan <darzhaev@gmail.com>
2019-11-15 22:14:58 +0900

    улучшен код
    вместо update используется обращение по ключу, а также приведен код к стандарту pep8, а также вынесена переменная название файла

--- format_list.py
+++ format_list.py
@@ -1,17 +1,17 @@
-def get_production():
-	with open("action.txt", "r", encoding='utf-8-sig') as rose_file:
+def get_production(filename):
+	with open(filename, "r", encoding='utf-8-sig') as rose_file:
 		rose_list = rose_file.read()
 	eng_list = rose_list.replace('Название: ', 'title:').replace('Сорт: ', 'sort:').replace('Цена: ', 'price:')
-	list_by_branch = eng_list.replace('Картинка: ', 'image:').replace('Выгодное предложение','action:True').split('#')
+	list_by_branch = eng_list.replace('Картинка: ', 'image:').replace('Выгодное предложение' , 'action:True').split('#')
 	list_by_branch.remove('')
 	production = {}
 	for branch_txt in list_by_branch:
-		branch_list= branch_txt.split('\n\n\n')
+		branch_list = branch_txt.split('\n\n\n')
 		product_list_data = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
 		all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list_data]
-		production.update({branch_list[0]: all_tag_dict})
+		production[branch_list[0]] = all_tag_dict
 	return production
 
 
 if __name__ == '__main__':
-	print(get_production())
+	print(get_production('action.txt'))
