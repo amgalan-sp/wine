d87138d5dd297f1a57f7f4168e501d1ef2c84180
Amgalan <darzhaev@gmail.com>
2019-11-19 00:57:25 +0900

    Исправлено название данных с продукцией

--- format_list.py
+++ format_list.py
@@ -1,17 +1,17 @@
-def get_production(filename):
+def get_products(filename):
     with open(filename, "r", encoding='utf-8-sig') as rose_file:
         rose_list = rose_file.read()
     eng_list = rose_list.replace('Название: ', 'title:').replace('Сорт: ', 'sort:').replace('Цена: ', 'price:')
     list_by_branch = eng_list.replace('Картинка: ', 'image:').replace('Выгодное предложение' , 'action:True').split('#')
     list_by_branch.remove('')
-    production = {}
+    products = {}
     for branch_txt in list_by_branch:
         branch_list = branch_txt.split('\n\n\n')
         product_list_data = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
         all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list_data]
-        production[branch_list[0]] = all_tag_dict
-    return production
+        products[branch_list[0]] = all_tag_dict
+    return products
 
 
 if __name__ == '__main__':
-    print(get_production('assortment.txt'))
+    print(get_products('assortment.txt'))
