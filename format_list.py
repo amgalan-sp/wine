54b953dba046cb4ced2703e14f45d72ec21c3565
Amgalan <darzhaev@gmail.com>
2019-11-20 00:50:31 +0900

    Очищен код
    Код избавлен от повторов

--- format_list.py
+++ format_list.py
@@ -1,15 +1,15 @@
+from read_file import get_read_file
+
 def get_products(some_list):
     eng_list = some_list.replace('Название: ', 'title:').replace('Сорт: ', 'sort:').replace('Цена: ', 'price:')
-    list_by_branch = eng_list.replace('Картинка: ', 'image:').replace('Выгодное предложение' , 'action:True').split('#')
+    list_by_branch = eng_list.replace('Картинка: ', 'image:').replace('Выгодное предложение', 'action:True').split('#')
     list_by_branch.remove('')
     products = {}
     for branch_txt in list_by_branch:
-        branch_list = branch_txt.split('\n\n\n')
-        product_list = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
-        all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list]
-        products[branch_list[0]] = all_tag_dict
+        product_list = [branch.split('\n') for branch in branch_txt.split('\n\n\n')[1].split('\n\n')]
+        products[branch_txt.split('\n\n\n')[0]] = [dict([tag.split(':') for tag in product]) for product in product_list]
     return products
 
 
 if __name__ == '__main__':
-    print(get_products('assortment.txt'))
+    print(get_products(get_read_file('assortment.txt')))
