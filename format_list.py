2c9fc013141dc77ad8deb458b8fee1a233b73f7c
Amgalan <darzhaev@gmail.com>
2019-11-20 00:39:38 +0900

    Форматирован код
    Подчищены строки кода от лишних наименований (data)

--- format_list.py
+++ format_list.py
@@ -5,8 +5,8 @@ def get_products(some_list):
     products = {}
     for branch_txt in list_by_branch:
         branch_list = branch_txt.split('\n\n\n')
-        product_list_data = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
-        all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list_data]
+        product_list = [branch.split('\n') for branch in branch_list[1].split('\n\n')]
+        all_tag_dict = [dict([tag.split(':') for tag in product]) for product in product_list]
         products[branch_list[0]] = all_tag_dict
     return products
 
